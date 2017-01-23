0f5b7863078b166ad8c0fa4d6eebf1b3b99f3254
Marcin Gornik <marcin.gornik@gmail.com>
2017-01-23 18:17:02 +0100

    Add notifying PagerDuty on network stall

--- notifications.py
+++ notifications.py
@@ -4,9 +4,12 @@ import json
 import requests
 import sys
 import time
+import uuid
 
 
 SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
+PAGERDUTY_URL = "https://events.pagerduty.com/generic/2010-04-15/create_event.json"
+PAGERDUTY_KEY = "a0eba9950e1849cd9adde9b0b2598633"
 
 
 def handle_result(previous, current):
@@ -35,7 +38,19 @@ def _log_success(previous, current):
     print "All checks passed."
     print "Current leader height: {}".format(current["heights"]["leader"])
     print "Current follower height: {}".format(current["heights"]["follower"])
-
+    # TODO remove
+    height = current["heights"]["leader"]
+    prev_run = _format_timestamp(previous["timestamp"])
+    curr_run = _format_timestamp(current["timestamp"])
+    _trigger_pagerduty_alert(
+        "Factom network stalled at {}".format(height),
+        {
+            "heights": current["heights"],
+            "previous_run": prev_run,
+            "current_run": curr_run
+        },
+        incident_key
+    )
 
 def _log_first_run(current):
     print "First run, skipping checks."
@@ -83,12 +98,24 @@ def _notify_follower_stalled(previous, current):
 def _notify_network_stalled(previous, current):
     prev_run = _format_timestamp(previous["timestamp"])
     curr_run = _format_timestamp(current["timestamp"])
+    height = current["heights"]["leader"]
     _post_to_slack([
         "@everyone The network is stalled.",
-        "Current height: {}".format(current["heights"]["follower"]),
+        "Current height: {}".format(height),
         "Previous check at {}".format(prev_run),
         "Current check at {}".format(curr_run)
     ])
+    incident_key = previous["incident_key"] or str(uuid.uuid4())
+    current["incident_key"] = incident_key
+    _trigger_pagerduty_alert(
+        "Factom network stalled at {}".format(height),
+        {
+            "heights": current["heights"],
+            "previous_run": prev_run,
+            "current_run": curr_run
+        },
+        incident_key
+    )
 
 
 def _post_to_slack(lines):
@@ -100,9 +127,26 @@ def _post_to_slack(lines):
         }
     requests.post(SLACK_WEBHOOK, json.dumps(message))
 
+
+def _trigger_pagerduty_alert(description, details, incident_key):
+    message = {
+        "event_type": "trigger",
+        "client": "jenkins",
+        "service_key": PAGERDUTY_KEY,
+        "description": description,
+        "details": details
+    }
+
+    if incident_key:
+        message["incident_key"] = incident_key
+
+    requests.post(PAGERDUTY_URL, json.dumps(message))
+
+
 def _format_timestamp(ts):
     if ts is None:
         return ""
 
     dt = datetime.utcfromtimestamp(ts)
     return dt.strftime("%Y-%m-%dT%H:%M:%S")
+
